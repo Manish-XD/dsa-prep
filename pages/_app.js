b118e8d18c98a29424977b5d2bc194c98eda2280
Manish-XD <manish.pgl41@gmail.com>
2023-01-18 17:30:54 +0530

    created a new project

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,5 @@
-import '../styles/globals.css';
-import LoadingBar from 'react-top-loading-bar';
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
+import '@/styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  const [progress, setProgress] = useState(0);
-  const router = useRouter();
-
-  useEffect(() => {
-    router.events.on('routeChangeStart', ()=>{
-      setProgress(40)
-    })
-    router.events.on('routeChangeComplete', ()=>{
-      setProgress(100)
-    })
-  },[router.query]);
-
-  return <>
-  <LoadingBar
-      color='#ffffff'
-      waitingTime={400}
-      progress={progress}
-      onLoaderFinished={() => setProgress(0)}
-    />
-    <Component {...pageProps} />
-    </>
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
-
-export default MyApp
