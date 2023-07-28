76596dd3f42854ad5a14432b6adf1247209fc11b
Manish-XD <manish.pgl41@gmail.com>
2023-07-29 00:57:32 +0530

    bug fixing

--- pages/api/login.js
+++ pages/api/login.js
@@ -4,22 +4,26 @@ var CryptoJS = require("crypto-js");
 var jwt = require('jsonwebtoken');
 
 const handler = async (req,res) => {
-    if(req.method == 'POST'){
-        let user = await User.findOne({"email": req.body.email});
-        const bytes = CryptoJS.AES.decrypt(user.password, "secret123");
-        let decryptedPassword = bytes.toString(CryptoJS.enc.Utf8);
-        if(user){
-            if(req.body.email === user.email && req.body.password === decryptedPassword){
-                var token = jwt.sign({id: user._id}, 'jwtsecret');
-                res.status(200).json({success: true, token});
+    try {        
+        if(req.method == 'POST'){
+            let user = await User.findOne({"email": req.body.email});
+            const bytes = CryptoJS.AES.decrypt(user.password, "secret123");
+            let decryptedPassword = bytes.toString(CryptoJS.enc.Utf8);
+            if(user){
+                if(req.body.email === user.email && req.body.password === decryptedPassword){
+                    var token = jwt.sign({id: user._id}, 'jwtsecret');
+                    res.status(200).json({success: true, token});
+                }
+                res.status(400).json({error: "Invalid Email or Password"});
             }
-            res.status(400).json({error: "Invalid Email or Password"});
+            else{
+                res.status(400).json({error: "No user found"});
+            }
+        }else{
+            res.status(400).json({error: "This method is not allowed"});
         }
-        else{
-            res.status(400).json({error: "No user found"});
-        }
-    }else{
-        res.status(400).json({error: "This method is not allowed"});
+    } catch (error) {
+        res.status(400).json({error: "User not found"});
     }
 }
 
