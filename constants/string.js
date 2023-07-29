928da9223d5f5c7da42ed844ee22d1127bd1ad57
Manish-XD <manish.pgl41@gmail.com>
2023-07-29 09:15:22 +0530

    added string questions

--- constants/string.js
+++ constants/string.js
@@ -1,16 +1,156 @@
 export const string = [
     {
         id: 1,
-        title: 'string ques 1',
+        title: 'Valid Palindrome',
         status: 0,
-        link: 'https://www.google.com/',
+        link: 'https://leetcode.com/problems/valid-palindrome/',
         level: 'Easy'
     },
     {
         id: 2,
-        title: 'string ques 2',
+        title: 'Valid Anagram',
         status: 0,
-        link: 'https://www.google.com/',
+        link: 'https://leetcode.com/problems/valid-anagram/',
+        level: 'Easy'
+    },
+    {
+        id: 3,
+        title: 'Valid Parenthesis',
+        status: 0,
+        link: 'https://leetcode.com/problems/valid-parentheses/',
+        level: 'Easy'
+    },
+    {
+        id: 4,
+        title: 'Remove Consecutive characters',
+        status: 0,
+        link: 'https://practice.geeksforgeeks.org/problems/consecutive-elements2306/1',
+        level: 'Easy'
+    },
+    {
+        id: 5,
+        title: 'Longest common prefix',
+        status: 0,
+        link: 'https://leetcode.com/problems/longest-common-prefix/',
+        level: 'Easy'
+    },
+    {
+        id: 6,
+        title: 'Convert sentence to equivalent mobile numeric keypad sequence',
+        status: 0,
+        link: 'https://www.geeksforgeeks.org/convert-sentence-equivalent-mobile-numeric-keypad-sequence/',
+        level: 'Easy'
+    },
+    {
+        id: 7,
+        title: 'Print all the duplicates in the input string',
+        status: 0,
+        link: 'https://www.geeksforgeeks.org/print-all-the-duplicates-in-the-input-string/',
+        level: 'Easy'
+    },
+    {
+        id: 8,
+        title: 'Longest substring without repeating characters',
+        status: 0,
+        link: 'https://leetcode.com/problems/longest-substring-without-repeating-characters/',
         level: 'Medium'
-    }
+    },
+    {
+        id: 9,
+        title: 'Longest repeating character replacement',
+        status: 0,
+        link: 'https://leetcode.com/problems/longest-repeating-character-replacement/',
+        level: 'Medium'
+    },
+    {
+        id: 10,
+        title: 'Group Anagrams',
+        status: 0,
+        link: 'https://leetcode.com/problems/group-anagrams/',
+        level: 'Medium'
+    },
+    {
+        id: 11,
+        title: 'Longest Palindromic substring',
+        status: 0,
+        link: 'https://leetcode.com/problems/longest-palindromic-substring/',
+        level: 'Medium'
+    },
+    {
+        id: 12,
+        title: 'Palindromic Substrings',
+        status: 0,
+        link: 'https://leetcode.com/problems/palindromic-substrings/',
+        level: 'Medium'
+    },
+    {
+        id: 13,
+        title: 'Next Permutation',
+        status: 0,
+        link: 'https://practice.geeksforgeeks.org/problems/next-permutation5226/1',
+        level: 'Medium'
+    },
+    {
+        id: 14,
+        title: 'Count palindromic subsequences',
+        status: 0,
+        link: 'https://practice.geeksforgeeks.org/problems/count-palindromic-subsequences/1',
+        level: 'Medium'
+    },
+    {
+        id: 15,
+        title: 'Smallest Window in a String Containing all the Characters of Another String',
+        status: 0,
+        link: 'https://practice.geeksforgeeks.org/problems/smallest-window-in-a-string-containing-all-the-characters-of-another-string-1587115621/1',
+        level: 'Medium'
+    },
+    {
+        id: 16,
+        title: 'Wildcard String Matching',
+        status: 0,
+        link: 'https://practice.geeksforgeeks.org/problems/wildcard-string-matching1126/1',
+        level: 'Medium'
+    },
+    {
+        id: 17,
+        title: 'Longest Prefix Suffix',
+        status: 0,
+        link: 'https://practice.geeksforgeeks.org/problems/longest-prefix-suffix2527/1',
+        level: 'Medium'
+    },
+    {
+        id: 18,
+        title: 'Rabin-Karp Algorithm for Pattern Searching',
+        status: 0,
+        link: 'https://www.geeksforgeeks.org/rabin-karp-algorithm-for-pattern-searching/',
+        level: 'Medium'
+    },
+    {
+        id: 19,
+        title: 'Transform One String to Another using Minimum Number of Given Operation',
+        status: 0,
+        link: 'https://www.geeksforgeeks.org/transform-one-string-to-another-using-minimum-number-of-given-operation/',
+        level: 'Medium'
+    },
+    {
+        id: 20,
+        title: 'Minimum Window Substring',
+        status: 0,
+        link: 'https://leetcode.com/problems/minimum-window-substring/',
+        level: 'Hard'
+    },
+    {
+        id: 21,
+        title: 'Boyer Moore Algorithm for Pattern Searching',
+        status: 0,
+        link: 'https://www.geeksforgeeks.org/boyer-moore-algorithm-for-pattern-searching/',
+        level: 'Hard'
+    },
+    {
+        id: 22,
+        title: 'Word Wrap',
+        status: 0,
+        link: 'https://practice.geeksforgeeks.org/problems/word-wrap1646/1',
+        level: 'Hard'
+    },
 ];
